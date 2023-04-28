091342b78a05e43cc7316d4e76ceac1160c6772a
Adriano Brito de Mendonça <62914181+adriano-mendonca@users.noreply.github.com>
2023-04-28 19:15:57 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,20 @@
+function toRadians(graus) {
+  return (graus * Math.PI) / 180;
+}
+
+function reta(ponto1, ponto2) {
+  circTerra = 40075000;
+  distanciaY = (ponto1['latitude'] * ponto2['latitude'] * circTerra) / 360;
+
+  fatorPonto1 = Math.cos(toRadians(ponto1['latitude']));
+  fatorPonto2 = Math.cos(toRadians(ponto2['latitude']));
+
+  distanciaX =
+    ((ponto1['longitude'] * fatorPonto1 - ponto2['longitude'] * fatorPonto2) *
+      circTerra) /
+    360;
+
+  distancia = Math.sqrt(distanciaX ** 2 + distanciaY ** 2);
+
+  return distancia;
+}
