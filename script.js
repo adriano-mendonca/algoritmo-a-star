881cd89ffa2846886a6c264360867a6834b7e425
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-05-11 19:54:25 -0300

    Melhoria na representação dos pontos
    Alteração na forma de armazenar as distâncias dos pontos adjacentes.

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const pontosJSON = fs.readFileSync('./pontos.json');
 
 const pontos = JSON.parse(pontosJSON);
 
-let fronteira = [];
-
 function toRadians(graus) {
   return (graus * Math.PI) / 180;
 }
@@ -50,14 +48,18 @@ G: é o custo do movimento. Ou seja, a distância de um ponto ao outro.
 H: É o custo estimado de movimento para mover de determinado ponto até o ponto final.
 
 1- Adicionar o ponto inicial a LISTA ABERTA
+*/
+let fronteira = []; // Lista Aberta
+let avaliados = []; // Lita Fechada
 
+/*
 2- Procurar o menor custo F na LISTA ABERTA
  - Mover para a LISTA FECHADA
  - Para cada ponto adjacente {
   Se não é passável ou se tiver na lista fechada, ignore.
   Caso contrário {
     Se não estiver na lista aberta, adicione-o. Faça o ponto atual o pai deste. Grave os custos F, G e H do ponto.
-    Se já estiver na lista aberta, confere para ver se este caminho para aquele ponto é melhor, usando o custo G como medida. Um valor G mais baixo mostra que este é u caminho melhor. Nesse caso, mude o pai do ponto para o ponto atual, e recalcule os valores de G e F do ponto. Se você está mantendo sua lista aberta ordenada por F, você pode precisar reordenar a lista para corresponder a mudança.
+    Se já estiver na lista aberta, confere para ver se este caminho para aquele ponto é melhor, usando o custo G como medida. Um valor G mais baixo mostra que este é um caminho melhor. Nesse caso, mude o pai do ponto para o ponto atual, e recalcule os valores de G e F do ponto. Se você está mantendo sua lista aberta ordenada por F, você pode precisar reordenar a lista para corresponder a mudança.
   }
   Pare quando {
     Acrescente o ponto alvo à lista fechada o que determina oque o caminho foi achado ou,
