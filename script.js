4bedfc652ebc3271bd68ff556469e883689c88bf
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-05-11 19:28:09 -0300

    Atualização nas funções principais
    Melhorando a função de calcular a distância em linha reta, e implementação da função de verificação de existência na fronteira

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 const fs = require('fs');
 
-const pontosJSON = fs.readFileSync('./pontos.json')
+const pontosJSON = fs.readFileSync('./pontos.json');
 
-const pontos = JSON.parse(pontosJSON)
+const pontos = JSON.parse(pontosJSON);
 
-let fronteira = []
+let fronteira = [];
 
 function toRadians(graus) {
   return (graus * Math.PI) / 180;
 }
 
-function calcularDistancia(lat1, lon1, lat2, lon2) {
+function distanceStraight(point1, point2) {
   const R = 6371; // Raio médio da Terra em quilômetros
 
-  const dLat = toRadians(lat2 - lat1);
-  const dLon = toRadians(lon2 - lon1);
+  const dLat = toRadians(point2.latitude - point1.latitude);
+  const dLon = toRadians(point2.longitude - point1.longitude);
 
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(point1.latitude)) *
+      Math.cos(toRadians(point2.latitude)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   const distancia = R * c * 1000; // Distância em metros
   return distancia;
+}
+
+function isInedit(ponto, lista) {
+  // Verifica se o ponto é inédito na fronteira
+  let saida = true;
+  lista.map((item) => {
+    if (item['id'] == ponto['id']) {
+      saida = false;
+      return;
+    }
+  });
+  return saida;
 }
 
 /* 
@@ -31,7 +46,7 @@ A chave para determinar quais pontos usar quando estiver procurando o caminho é
 
 F = G + H
 
-G: é o custo do movimento.
+G: é o custo do movimento. Ou seja, a distância de um ponto ao outro.
 H: É o custo estimado de movimento para mover de determinado ponto até o ponto final.
 
 1- Adicionar o ponto inicial a LISTA ABERTA
@@ -45,7 +60,7 @@ H: É o custo estimado de movimento para mover de determinado ponto até o ponto
     Se já estiver na lista aberta, confere para ver se este caminho para aquele ponto é melhor, usando o custo G como medida. Um valor G mais baixo mostra que este é u caminho melhor. Nesse caso, mude o pai do ponto para o ponto atual, e recalcule os valores de G e F do ponto. Se você está mantendo sua lista aberta ordenada por F, você pode precisar reordenar a lista para corresponder a mudança.
   }
   Pare quando {
-    Acrescente o ponto alvo à lista fecada o que determina oque o caminho foi achado ou,
+    Acrescente o ponto alvo à lista fechada o que determina oque o caminho foi achado ou,
     Não ache o quadrado alvo, e a lista aberta está vazia.
   }
  }
