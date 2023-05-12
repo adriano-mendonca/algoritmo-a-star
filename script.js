3558eb736d5a5c46a8ec4dda364659a6e5d8b200
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2023-05-12 09:48:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
+const fs = require('fs')
+
+const pontosJSON = fs.readFileSync('./pontos.json')
+
+const pontos = JSON.parse(pontosJSON)
+
 function toRadians(graus) {
   return (graus * Math.PI) / 180;
 }
 
-function reta(ponto1, ponto2) {
-  circTerra = 40075000;
-  distanciaY = (ponto1['latitude'] * ponto2['latitude'] * circTerra) / 360;
+function distanceStraight(point1, point2) {
+  const R = 6371; // Raio médio da Terra em quilômetros
 
-  fatorPonto1 = Math.cos(toRadians(ponto1['latitude']));
-  fatorPonto2 = Math.cos(toRadians(ponto2['latitude']));
+  const dLat = toRadians(point2['latitude'] - point1['latitude']);
+  const dLon = toRadians(point2['longitude'] - point1['longitude']);
 
-  distanciaX =
-    ((ponto1['longitude'] * fatorPonto1 - ponto2['longitude'] * fatorPonto2) *
-      circTerra) /
-    360;
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(toRadians(point1['latitude'])) * Math.cos(toRadians(point2['latitude'])) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   const distancia = R * c * 1000; // Distância em metros
   return distancia;
 }
 
+function isInedit(point, listaAberta = fronteira) {
+  let saida = true;
+  listaAberta.map(item => {
+    if(item['id'] == point['id']){
+      saida = false
+      return
+    }
+  })
+
+  return saida
+}
+
+function searchAdj(name, lista = pontos) {
+  let res = ''
+  lista.map(item => {
+    if(item['nome'] == name) {
+      res = item
+    }
+  })
+
+  return item
+}
+
 /* 
 A chave para determinar quais pontos usar quando estiver procurando o caminho é:
 F = G + H
-G: é o custo do movimento.
+G: é o custo do movimento. Ou seja, a distância entre os pontos
 H: É o custo estimado de movimento para mover de determinado ponto até o ponto final.
 1- Adicionar o ponto inicial a LISTA ABERTA
+*/
+
+const fronteira = [] // Lista aberta
+const visitados = [] // lista fechada
+const destino = 5
+let atual = 0
+
+fronteira.push(pontos[0])
+
+atual = fronteira[0]
+
+if(atual['id'] == destino){
+  console.log('Chegou ao destino final')
+} else {
+  atual['adjacentes'].map(pontoAdjacente => {
+    if(isInedit(pontoAdjacente)){
+      fronteira.push(pontoAdjacente)
+    }
+  })
+}
+
+// Calcular o custo
+// Ordernar pelo custo na lista aberta
+
+/*
 2- Procurar o menor custo F na LISTA ABERTA
  - Mover para a LISTA FECHADA
  - Para cada ponto adjacente {
   Se não é passável ou se tiver na lista fechada, ignore.
   Caso contrário {
     Se não estiver na lista aberta, adicione-o. Faça o ponto atual o pai deste. Grave os custos F, G e H do ponto.
-    Se já estiver na lista aberta, confere para ver se este caminho para aquele ponto é melhor, usando o custo G como medida. Um valor G mais baixo mostra que este é u caminho melhor. Nesse caso, mude o pai do ponto para o ponto atual, e recalcule os valores de G e F do ponto. Se você está mantendo sua lista aberta ordenada por F, você pode precisar reordenar a lista para corresponder a mudança.
+    Se já estiver na lista aberta, confere para ver se este caminho para aquele ponto é melhor, usando o custo G como medida. Um valor G mais baixo mostra que este é um caminho melhor. Nesse caso, mude o pai do ponto para o ponto atual, e recalcule os valores de G e F do ponto. Se você está mantendo sua lista aberta ordenada por F, você pode precisar reordenar a lista para corresponder a mudança.
   }
   Pare quando {
-    Acrescente o ponto alvo à lista fecada o que determina oque o caminho foi achado ou,
+    Acrescente o ponto alvo à lista fechada o que determina oque o caminho foi achado ou,
     Não ache o quadrado alvo, e a lista aberta está vazia.
   }
  }
